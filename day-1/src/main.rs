c81d9085da3ca95eba09d0db92c2639f28aa8317
Daniel Jackson <danieljackson97123@gmail.com>
2025-12-01 18:49:58 +0000

    wip

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -6,53 +6,58 @@ struct Output {
     count_of_times_passed_0: i32,
 }
 
-const STARTING_VALUE: Output = Output {
-    current_position: 50,
-    count_of_times_reached_0: 0,
-    count_of_times_passed_0: 0,
-};
+impl Default for Output {
+    fn default() -> Self {
+        Output {
+            current_position: 50,
+            count_of_times_reached_0: 0,
+            count_of_times_passed_0: 0,
+        }
+    }
+}
 
 const MAX_POSITION: i32 = 100;
 
-fn get_movement_value(chars : &[char]) -> i32 {
-    let movement: String = chars.iter().collect();
-    movement.parse().unwrap_or(0)
+fn get_number_from_chars(chars: &[char]) -> i32 {
+    chars
+        .iter()
+        .collect::<String>()
+        .parse()
+        .expect("Failed to parse number")
+}
+
+fn fold_line(acc: Output, line: &str) -> Output {
+    let chars: Vec<char> = line.chars().collect();
+    match &chars[..] {
+        [direction, num @ ..] => {
+            let movement = match *direction {
+                'L' => -get_number_from_chars(num),
+                'R' => get_number_from_chars(num),
+                _ => panic!("Unknown direction: {}", direction),
+            };
+            let total_movement = acc.current_position + movement;
+
+            let new_value = total_movement % MAX_POSITION;
+
+            Output {
+                current_position: new_value,
+                count_of_times_reached_0: acc.count_of_times_reached_0 + if new_value == 0 { 1 } else { 0 },
+                count_of_times_passed_0: acc.count_of_times_passed_0,
+            }
+        }
+        _ => acc,
+    }
 }
 
 fn main() {
     match fs::read_to_string("./day-1/assets/input.txt") {
         Ok(contents) => {
-            let output = contents.split("\n").fold(STARTING_VALUE, |acc, line| {
-                let chars: Vec<char> = line.chars().collect();
-                return match &chars[..] {
-                    [direction, num @ ..] => {
-                        let movement = if *direction == 'L' {
-                            -get_movement_value(num)
-                        } else {
-                            get_movement_value(num)
-                        };
-                        let total_movement = acc.current_position + movement;
-                        let times_passed_0 = total_movement / MAX_POSITION;
-                        println!(
-                            "Direction: {}, Movement: {}, New Position: {}, Times Passed 0: {}",
-                            direction, movement, total_movement, times_passed_0
-                        );
-                        let new_value = total_movement % MAX_POSITION;
-                        Output {
-                            current_position: new_value,
-                            count_of_times_reached_0: acc.count_of_times_reached_0 + if new_value == 0 { 1 } else { 0 },
-                            count_of_times_passed_0: acc.count_of_times_passed_0 + times_passed_0,
-                        }
-                    }
-                    _ => {
-                        println!("No movement");
-                        acc
-                    }
-                };
-            });
+            let output = contents.split("\n").fold(Default::default(), fold_line);
             println!(
                 "Final Position: {}, Times Reached 0: {}, Times Passed 0: {}",
-                output.current_position, output.count_of_times_reached_0, output.count_of_times_passed_0
+                output.current_position,
+                output.count_of_times_reached_0,
+                output.count_of_times_passed_0
             );
         }
         Err(e) => eprintln!("Error reading file: {}", e),
